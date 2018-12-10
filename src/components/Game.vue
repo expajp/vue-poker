9e198982b329d2674e5430e5c3bd9c517ac0e86f
Shu OGAWARA <expajp@gmail.com>
2018-12-10 23:01:07 +0900

    implement Game component

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,9 +1,42 @@
 <template>
-    <div id="game"></div>
+    <div id="game">
+        <dealer ref="dealer" @result="postexec" />
+        <div class="message">
+            {{ mainMessage }}
+        </div>
+        <player @stand="stand" :showButtons="showButtons" />
+        <div class="message result">
+            {{ resultMessage }}
+        </div>
+    </div>
 </template>
 
 <script>
-    export default {
-        name: 'game'
+import Dealer from './Dealer'
+import Player from './Player'
+
+export default {
+    name: 'game',
+    components: { Dealer, Player },
+    data(){
+        return {
+            mainMessage: 'Welcome to Black Jack',
+            playersResult: 0,
+            dealersResult: 0,
+            showButtons: 0
+        }
+    },
+    methods: {
+        stand: function(playersResult){},
+        postexec: function(dealersResult){},
+    },
+    computed: {
+        resultMessage: function(){
+            if(this.showButtons) return ''
+            if(this.playersResult > this.dealersResult || this.dealersResult == 'Burst!!') return 'You Win'
+            if(this.playersResult < this.dealersResult || this.playersResult == 'Burst!!') return 'You Lose'
+            return 'Draw'
+        }
     }
+}
 </script>
