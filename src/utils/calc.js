a765e8dc67150e595e640111c6455929e4a58b85
Shu OGAWARA <expajp@gmail.com>
2019-01-02 02:04:01 +0900

    change implementation of logic for distinguishing yaku

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -6,17 +6,29 @@ import score from './score.js'
  Rubyで言えば Array#any?
 */
 export default hand => {
-    const ret = Object.assign(numbersCounter(hand), flashChecker(hand), straightChecker(hand)) 
-    console.log(JSON.stringify(ret))
+    const yaku = getYaku(hand)
+    console.log(JSON.parse(JSON.stringify(yaku)))
+}
 
-    // TODO スートの強さを判定するロジック
-    // TODO ストレートのときのスコア
+function getYaku(hand){
+    const checkersResult = Object.assign(numbersCounter(hand), flashChecker(hand), straightChecker(hand), scoreChecker(hand))
+    console.log(JSON.parse(JSON.stringify(checkersResult)))
+    let ret = { yaku: 'None', score: 0, suit: '' }
+
+    if(checkersResult.pairs == 1) ret.yaku = 'OnePair'
+    if(checkersResult.pairs == 2) ret.yaku = 'TwoPair'
+    if(checkersResult.threeCard) ret.yaku = 'ThreeCard'
+    if(checkersResult.straight) ret.yaku = 'Straight'
+    if(checkersResult.flash) ret.yaku = 'Flash'
+    if(checkersResult.pairs == 1 && checkersResult.threeCard) ret.yaku = 'FullHouse'
+    if(checkersResult.fourCard) ret.yaku = 'FourCard'
+    if(checkersResult.straight && checkersResult.flash) ret.yaku = 'StraightFlash'
+    if(checkersResult.straight && checkersResult.flash && checkersResult.score == 12) ret.yaku = 'RoyalStraightFlash'
 
     return ret
 }
-
 function numbersCounter(hand) {
-    let ret = { pairs: 0, threeCard: false, fourCard: false, maxNumber: 0 }
+    let ret = { pairs: 0, threeCard: false, fourCard: false, numbersScore: 0, numbersSuit: '' }
     
     const numbers = hand.map( card => { return card.number } ).sort()
     const numbersCounter = [...new Set(numbers)]
@@ -26,13 +38,16 @@ function numbersCounter(hand) {
         switch(count){
             case 2:
                 ret.pairs += 1
-                ret.maxNumber = (ret.maxNumber < score(number) ? score(number) : ret.maxNumber); break
+                ret.numbersSuit = (ret.numbersScore < score(number) ? score(number) : ret.numbersScore)
+                ret.numbersScore = (ret.numbersScore < score(number) ? score(number) : ret.numbersScore); break
             case 3: 
                 ret.threeCard = true
-                ret.maxNumber = score(number); break
+                ret.numbersSuit = ''
+                ret.numbersScore = score(number); break
             case 4: 
                 ret.fourCard = true
-                ret.maxNumber = score(number); break
+                ret.numbersSuit = ''
+                ret.numbersScore = score(number); break
             default: 
                 break
         }
@@ -41,12 +56,18 @@ function numbersCounter(hand) {
 }
 
 function flashChecker(hand) {
-    return { flash: [...new Set(hand.map(card => { card.suit } ))].length === 1, suit: hand[0].suit }
+    return { flash: [...new Set(hand.map(card => { card.suit } ))].length === 1, flashSuit: hand[0].suit }
 }
 
 function straightChecker(hand) {
-    const numbers = hand.map( card => { return card.number } ).sort((a, b) => { return score(b)-score(a) })
-    const maxIndex = numbers.length-1
-    return { straight: Math.abs(score(numbers[maxIndex])-score(numbers[0])) === maxIndex }
+    const sortedHand = hand.sort((a, b) => { return score(a.number)-score(b.number) })
+    const maxIndex = sortedHand.length-1
+    const scoreDiff = Math.abs(score(sortedHand[maxIndex].number)-score(sortedHand[0].number))
+
+    if(scoreDiff === maxIndex) return { straight: true, straightSuit: sortedHand[maxIndex].suit }
+    return { straight: false, straightSuit: '' }
 }
 
+function scoreChecker(hand) {
+    return { score: Math.max(hand.map(card => { return card.number })) }
+}
