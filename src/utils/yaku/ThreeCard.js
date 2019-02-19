fc166a850e9c0f0bd8adbfa0b024d2014af58fa3
Shu OGAWARA <expajp@gmail.com>
2019-02-19 20:03:49 +0900

    implemented ThreeCard

---
+++ src/utils/yaku/ThreeCard.js
@@ -0,0 +1,28 @@
+import AbstractYaku from './AbstractYaku.js'
+import deepcopyArray from '../deepcopyArray.js'
+import getScore from '../score.js'
+import getDifferenceArrays from '../getDifferenceArrays.js'
+
+// ThreeCardクラス
+export default class extends AbstractYaku {
+    constructor(hand){
+        super(hand)
+
+        const numbersArray = deepcopyArray(hand).map(card => card.number)
+        const numbersSet = [...new Set(deepcopyArray(numbersArray))]
+        this.tripledNumber = getDifferenceArrays(numbersArray, numbersSet)[0]
+    }
+
+    // 役のランクを返す
+    getYakuRank(){
+        return 3
+    }
+    // 役の名前を返す
+    getYakuName(){
+        return 'ThreeCard'
+    }
+    // 勝敗判定に使う数字を返す
+    getScore(){
+        return getScore(this.tripledNumber)
+    }
+}
