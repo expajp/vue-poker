8c10883f6d96cc59e7ca191ba831b17a7f41e8c3
Shu OGAWARA <expajp@gmail.com>
2019-03-10 16:30:55 +0900

    move repository to new one

---
+++ src_old/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <game></game>
+  </div>
+</template>
+
+<script>
+import Game from './components/Game'
+
+export default {
+  name: 'app',
+  components: { Game }
+}
+</script>
+
+<style lang="scss">
+</style>
