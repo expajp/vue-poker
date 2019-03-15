6f39b871ad2b12bbecba09b7e7eb6fdf2d367ed9
Shu OGAWARA <expajp@gmail.com>
2019-03-15 17:16:51 +0900

    making efforts to test Vue components by jest

--- tests/components/Card.test.js
+++ tests/components/Card.test.js
@@ -1,8 +1,10 @@
 import { shallowMount } from '@vue/test-utils'
 import Card from '../../src/components/Card.vue'
 
-test('test for test', () => {
+describe('test for test', () => {
     const wrapper = shallowMount(Card)
 
-    expect(wrapper.find('#hoge').text()).toEqual('ほげ')
+    it('renders hoge', () => {
+        expect(wrapper.find('#hoge').text()).toEqual('ほげ')
+    })
 })
