6ab768a1121002518adb5f7b2a2feb1cffd9e00c
Shu OGAWARA <expajp@gmail.com>
2019-04-03 21:22:44 +0900

    add test for deepcopyArray

---
+++ tests/unit/utils/deepcopyArray.spec.js
@@ -0,0 +1,11 @@
+import deepcopyArray from '@/utils/deepcopyArray'
+
+describe('正常系', () => {
+    const arr = [1, 2, 3]
+    const subject = deepcopyArray(arr)
+
+    it('中身はすべて同じだおオブジェクトIDは異なる', () => {
+        expect(subject).toEqual(arr)
+        expect(subject).not.toBe(arr)
+    })
+})
--- tests/unit/utils/getDifferenceArrays.spec.js
+++ tests/unit/utils/getDifferenceArrays.spec.js
@@ -3,6 +3,8 @@ import getDifferenceArrays from '@/utils/getDifferenceArrays'
 test('正常系', () => {
     const arr1 = [1, 2, 3]
     const arr2 = [2, 3, 4]
+    const subject = getDifferenceArrays(arr1, arr2)
 
-    expect(getDifferenceArrays(arr1, arr2)).toContain(1)
+    expect(subject).toContain(1)
+    expect(subject).toHaveLength(1)
 })
