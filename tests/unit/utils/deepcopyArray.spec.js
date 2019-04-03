ac2cb85919133d85b3f575e4f96cc95807f93bb4
Shu OGAWARA <expajp@gmail.com>
2019-04-03 21:23:29 +0900

    タイポの修正

--- tests/unit/utils/deepcopyArray.spec.js
+++ tests/unit/utils/deepcopyArray.spec.js
@@ -4,7 +4,7 @@ describe('正常系', () => {
     const arr = [1, 2, 3]
     const subject = deepcopyArray(arr)
 
-    it('中身はすべて同じだおオブジェクトIDは異なる', () => {
+    it('中身はすべて同じだがオブジェクトIDは異なる', () => {
         expect(subject).toEqual(arr)
         expect(subject).not.toBe(arr)
     })
