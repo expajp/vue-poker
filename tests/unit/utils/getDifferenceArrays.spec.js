c2d206c18c6a2fb6cfc421be2ef9c0a7a7f346b4
Shu OGAWARA <expajp@gmail.com>
2019-04-04 09:34:33 +0900

    modify getDifferenceArrays test simpler

--- tests/unit/utils/getDifferenceArrays.spec.js
+++ tests/unit/utils/getDifferenceArrays.spec.js
@@ -1,10 +1,11 @@
 import getDifferenceArrays from '@/utils/getDifferenceArrays'
 
-test('正常系', () => {
+describe('正常系', () => {
     const arr1 = [1, 2, 3]
     const arr2 = [2, 3, 4]
     const subject = getDifferenceArrays(arr1, arr2)
 
-    expect(subject).toContain(1)
-    expect(subject).toHaveLength(1)
+    it('中身が1のみの配列になる', () => {
+        expect(subject).toEqual([1])
+    })
 })
