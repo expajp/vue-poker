d2eaed078d1bdef9525221971eb5d283b782457d
Shu OGAWARA <expajp@gmail.com>
2019-04-08 09:49:41 +0900

    add tests for isEqualCards

--- tests/unit/utils/isEqualCards.spec.js
+++ tests/unit/utils/isEqualCards.spec.js
@@ -8,3 +8,21 @@ describe('一致する', () => {
         expect(isEqualCards(card_a, card_b)).toBeTruthy
     })
 })
+
+describe('スートが異なる', () => {
+    const card_a = { number: 1, suit: 'spade' }
+    const card_b = { number: 1, suit: 'heart' }
+
+    it('trueを返す', () => {
+        expect(isEqualCards(card_a, card_b)).toBeFalsy
+    })
+})
+
+describe('数字が異なる', () => {
+    const card_a = { number: 1, suit: 'spade' }
+    const card_b = { number: 2, suit: 'spade' }
+
+    it('trueを返す', () => {
+        expect(isEqualCards(card_a, card_b)).toBeFalsy
+    })
+})
