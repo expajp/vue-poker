5764a184d4f35972f24af7b2c4250489f63dc081
Shu OGAWARA <expajp@gmail.com>
2019-04-09 19:48:17 +0900

    add test for pickCard

--- src/utils/pickCard.js
+++ src/utils/pickCard.js
@@ -7,6 +7,8 @@ const deck = []
 
 export default (selected) => {
     let card = deck.splice(Math.floor(Math.random() * Math.floor(deck.length)), 1)[0]
+    if(card === undefined) return undefined
+
     card.selected = selected ? true : false
     return card
 }
---
+++ tests/unit/utils/pickCard.spec.js
@@ -0,0 +1,73 @@
+import deepcopyArray from '@/utils/deepcopyArray.js'
+import getUniqueArray from '@/utils/getUniqueArray.js'
+import pickCard from '@/utils/pickCard.js'
+
+// 52枚カードを引く
+let deck = []
+for(let i=0;i<26;i++) deck.push(pickCard(true))
+for(let i=0;i<26;i++) deck.push(pickCard(false))
+
+describe('数字とスートを持ったカードを引く', () => {
+    const card = deck[0]
+
+    it('数字とスートはともにundefinedでない', () => {
+        expect(card.number).not.toBeUndefined
+        expect(card.suit).not.toBeUndefined
+    })
+})
+
+describe('カードは52枚', () => {
+    it('52枚カードを引いたあとにpickCard()するとundefined', () => {
+        expect(pickCard()).toBeUndefined
+    })
+})
+
+describe('カードに重複なし', () => {
+    it('スペードのカードは重複なしの13枚', () => {
+        const spades = deepcopyArray(deck)
+                        .filter((card) => { return card.suit === 'spade' })
+                        .map((card) => { return card.number })
+
+        expect(spades.length).toBe(13)
+        expect(Math.min(...spades)).toBe(1)
+        expect(Math.max(...spades)).toBe(13)        
+    })
+    it('ハートのカードは重複なしの13枚', () => {
+        const hearts = deepcopyArray(deck)
+                        .filter((card) => { return card.suit === 'heart' })
+                        .map((card) => { return card.number })
+
+        expect(hearts.length).toBe(13)
+        expect(Math.min(...hearts)).toBe(1)
+        expect(Math.max(...hearts)).toBe(13)        
+    })
+    it('ダイヤのカードは重複なしの13枚', () => {
+        const diamonds = deepcopyArray(deck)
+                        .filter((card) => { return card.suit === 'diamond' })
+                        .map((card) => { return card.number })
+
+        expect(diamonds.length).toBe(13)
+        expect(Math.min(...diamonds)).toBe(1)
+        expect(Math.max(...diamonds)).toBe(13)        
+    })
+    it('クラブのカードは重複なしの13枚', () => {
+        const clubs = deepcopyArray(deck)
+                        .filter((card) => { return card.suit === 'club' })
+                        .map((card) => { return card.number })
+
+        expect(clubs.length).toBe(13)
+        expect(Math.min(...clubs)).toBe(1)
+        expect(Math.max(...clubs)).toBe(13)        
+    })
+})
+
+describe('指定したselectedに従ったcardが出てくる', () => {
+    it('trueを指定するとselected: true', () => {
+        expect(deck[0].selected).toBeTruthy
+    })
+    it('falseを指定するとselected: false', () => {
+        expect(deck[0].selected).toBeFalsy
+    })
+})
+
+
