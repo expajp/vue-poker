fa24f65582ba66c3ca130a725922461c7a482d67
Shu OGAWARA <expajp@gmail.com>
2019-05-13 23:45:17 +0900

    selectedを抽出する

--- tests/unit/components/Player.spec.js
+++ tests/unit/components/Player.spec.js
@@ -13,5 +13,18 @@ describe("正しく描画される", () => {
     it("ボタンが2個描画される", () => {
         expect(player.findAll("button").length).toBe(2)
     })
-  
+})
+
+describe("selectedが正しく抽出される", () => {
+    const hand = [
+        { number: 10, suit: 'spade', selected: false, hide: false },
+        { number: 11, suit: 'spade', selected: true,  hide: false },
+        { number: 12, suit: 'spade', selected: false, hide: false },
+        { number: 13, suit: 'spade', selected: false, hide: false },
+        { number:  1, suit: 'spade', selected: true,  hide: false }
+    ]
+
+    it("selectedがカードを2枚返す", () => {
+        expect(Player.computed.selected.call({ hand: hand }).length).toBe(2)
+    })
 })
