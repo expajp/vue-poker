2fd4adb764b81b85ab7745a93ebf529fc25b942f
Shu OGAWARA <expajp@gmail.com>
2019-05-21 00:35:01 +0900

    勝敗判定テストを3個実装

--- src/utils/judgingObjectFactory.js
+++ src/utils/judgingObjectFactory.js
@@ -1,8 +1,4 @@
-import getScore from './getScore.js'
-import cmpCardsByScoreAsc from './cmpCardsByScoreAsc.js'
-import deepcopyArray from './deepcopyArray.js'
-import getDifferenceArrays from './getDifferenceArrays.js'
-import getUniqueArray from './getUniqueArray.js'
+import Hand from './Hand'
 import { FourCard, None, RoyalStraightFlash, StraightFlash, TwoPair, Flash, FullHouse, OnePair, Straight, ThreeCard } from '../yaku'
 
 export default (playersHand) => {
@@ -26,60 +22,4 @@ function generateJudgingObject(hand){
     if(hand.isTwoPair()) return new TwoPair(hand)
     if(hand.isOnePair()) return new OnePair(hand)
     if(hand.hasNone()) return new None(hand)
-}
-
-class Hand {
-    constructor(arr){
-        // 手札に含まれるカード
-        this.cards = arr
-        
-        this.numbersArray = deepcopyArray(this.cards).map(card => card.number)
-        const numbersSet = getUniqueArray(this.numbersArray)
-
-        // 重複している数字のみ、かつ多重度が1つ減ったもの
-        this.doubledNumbers = getDifferenceArrays(this.numbersArray, numbersSet)
-
-        // 重複している数字
-        this.doubledNumbersSet = getUniqueArray(this.doubledNumbers)
-    }
-
-    getMaxScore(){
-        return Math.max(...deepcopyArray(this.numbersArray).map(number => getScore(number)))
-    }
-
-    isFlash(){
-        return getUniqueArray(this.cards.map(card => card.suit)).length === 1
-    }
-
-    isStraight(){
-        const sortedHand = deepcopyArray(this.cards).sort((a, b) => cmpCardsByScoreAsc(a, b))
-        const scoreDiff = Math.abs(getScore(sortedHand[4].number)-getScore(sortedHand[0].number))
-        const numbers = getUniqueArray(deepcopyArray(sortedHand).map(card => card.number))
-
-        return scoreDiff === 4 && numbers.length === 5
-    }
-
-    hasNone(){
-        return this.doubledNumbers.length === 0
-    }
-
-    isOnePair(){
-        return this.doubledNumbersSet.length === 1 && this.doubledNumbers.length === 1
-    }
-
-    isTwoPair(){
-        return this.doubledNumbersSet.length === 2 && this.doubledNumbers.length === 2
-    }
-    
-    isThreeCard(){
-        return this.doubledNumbersSet.length === 1 && this.doubledNumbers.length === 2
-    }
-    
-    isFullHouse(){
-        return this.doubledNumbersSet.length === 2 && this.doubledNumbers.length === 3
-    }
-
-    isFourCard(){
-        return this.doubledNumbersSet.length === 1 && this.doubledNumbers.length === 3
-    }
 }
--- tests/unit/components/Game.spec.js
+++ tests/unit/components/Game.spec.js
@@ -1,5 +1,7 @@
 import { mount } from "@vue/test-utils"
 import Game from "@/components/Game.vue"
+import Hand from "@/utils/Hand"
+import { FourCard, None, RoyalStraightFlash, StraightFlash, TwoPair, Flash, FullHouse, OnePair, Straight, ThreeCard } from '@/yaku'
 
 describe("正しく描画される", () => {
     const game = mount(Game)
@@ -9,7 +11,7 @@ describe("正しく描画される", () => {
     })
 })
 
-describe.only("resultMessageが正しく動作する", () => {
+describe("resultMessageが正しく動作する", () => {
     const game = mount(Game)
 
     it("showButtonsがtrueのときは空文字列", () => {
@@ -22,17 +24,65 @@ describe.only("resultMessageが正しく動作する", () => {
     })
 })
 
-describe("勝敗を判定する", () => {
+describe.only("勝敗を判定する", () => {
     it("両者Noneならば引き分け", () => {
+        const game = mount(Game)
+        const playersHand = [
+            { number:  7, suit: 'spade' },
+            { number:  9, suit: 'diamond' },
+            { number: 11, suit: 'clover' },
+            { number: 13, suit: 'heart' },
+            { number:  1, suit: 'spade' }
+        ]
+        const dealersHand = [
+            { number:  6, suit: 'spade' },
+            { number:  8, suit: 'diamond' },
+            { number: 10, suit: 'clover' },
+            { number: 12, suit: 'heart' },
+            { number:  1, suit: 'spade' }
+        ]
         
+        expect(game.vm.judgeVictoryOrDefeat(new None(playersHand), new None(dealersHand))).toBe('Draw')
     })
     
     it("役が異なるならば強いほうが勝利", () => {
+        const game = mount(Game)
+        const playersHand = new Hand([
+            { number:  7, suit: 'spade' },
+            { number:  8, suit: 'spade' },
+            { number:  9, suit: 'spade' },
+            { number: 10, suit: 'spade' },
+            { number: 11, suit: 'spade' }
+        ])
+        const dealersHand = new Hand([
+            { number: 10, suit: 'spade' },
+            { number: 11, suit: 'spade' },
+            { number: 12, suit: 'spade' },
+            { number: 13, suit: 'spade' },
+            { number:  1, suit: 'spade' }
+        ])
         
+        expect(game.vm.judgeVictoryOrDefeat(new StraightFlash(playersHand), new RoyalStraightFlash(dealersHand))).toBe('You Lose')
     })
+    
+    it("ストレートフラッシュどうしで数字を比較して大きいほうが勝利", () => {
+        const game = mount(Game)
+        const playersHand = new Hand([
+            { number:  9, suit: 'spade' },
+            { number: 10, suit: 'spade' },
+            { number: 11, suit: 'spade' },
+            { number: 12, suit: 'spade' },
+            { number: 13, suit: 'spade' }
+        ])
+        const dealersHand = new Hand([
+            { number:  8, suit: 'spade' },
+            { number:  9, suit: 'spade' },
+            { number: 10, suit: 'spade' },
+            { number: 11, suit: 'spade' },
+            { number: 12, suit: 'spade' }
+        ])
 
-    it("役が同じならば、数字を比較して大きいほうが勝利", () => {
-
+        expect(game.vm.judgeVictoryOrDefeat(new StraightFlash(playersHand), new StraightFlash(dealersHand))).toBe('You Win')
     })
 
     it("ツーペアどうして大きい方の数字が同じならば、もう一つの数字が大きい方が勝利", () => {
