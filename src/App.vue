b74faae519a9d36a534e2b2773107fa23c381d4d
Shu OGAWARA <expajp@gmail.com>
2019-06-04 01:06:34 +0900

    add credit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
     <game></game>
+    <credit></credit>
   </div>
 </template>
 
 <script>
 import Game from './components/Game'
+import Credit from './components/Credit'
 
 export default {
   name: 'app',
-  components: { Game }
+  components: { Game, Credit }
 }
 </script>
 
---
+++ src/components/Credit.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="credit">
+      <span>Fork from <a href="https://github.com/t-kojima/blackjack-vue" target="_blank">t-kojima/blackjack-vue</a></span>
+      <span>Created by <a href="https://twitter.com/expajp" target="_blank">@expajp</a></span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'credit'
+}
+</script>
+
+<style>
+#credit {
+    text-align: right;
+}
+
+#credit span {
+  margin: 0 0.5em;
+}
+</style>
